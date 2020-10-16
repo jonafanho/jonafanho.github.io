f99763e9016b7a204926aaec82fa183a9e7fa678
Jonathan Ho <jonafanho@gmail.com>
2020-10-16 15:29:37 -0700

    Canvas drawing logic.

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const NEW_TIMELINE = {
 			name: "Example Event",
 			description: "This is an event description!",
 			start: 1997,
-			end: 2015
+			end: 2015,
+			color: "#CCCCCC"
 		}
 	]
 }
+const EVENT_HEIGHT = 42;
 
 function setup() {
 	"use strict";
@@ -26,7 +28,7 @@ function setup() {
 		}
 
 		newTimeline(event) {
-			this.setState({timeline: JSON.parse(JSON.stringify(NEW_TIMELINE))});
+			this.setState({timeline: JSON.parse(JSON.stringify(NEW_TIMELINE))}, this.drawCanvas);
 		}
 
 		deleteEvent(event, index) {
@@ -40,6 +42,7 @@ function setup() {
 			document.querySelector("#input_event_description").value = editEvent["description"];
 			document.querySelector("#input_event_start").value = editEvent["start"].toString();
 			document.querySelector("#input_event_end").value = editEvent["end"].toString();
+			document.querySelector("#input_event_color").value = editEvent["color"];
 			this.setState({editing_index: index});
 		}
 
@@ -48,12 +51,12 @@ function setup() {
 			document.querySelector("#input_event_description").value = "";
 			document.querySelector("#input_event_start").value = "";
 			document.querySelector("#input_event_end").value = "";
+			document.querySelector("#input_event_color").value = "#" + Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, "0");
 			this.setState({editing_index: -1});
 		}
 
 		editNumeric(event) {
-			const id = event.target.id;
-			document.querySelector(`#${id}`).value = document.querySelector(`#${id}`).value.replace(/[^0-9]/g, "");
+			event.target.value = event.target.value.replace(/[^0-9-]/g, "");
 		}
 
 		addEvent(event) {
@@ -68,12 +71,63 @@ function setup() {
 				name: document.querySelector("#input_event_name").value,
 				description: document.querySelector("#input_event_description").value,
 				start: isNaN(startYear) ? 2000 : Math.min(startYear, endYear),
-				end: isNaN(endYear) ? 2000 : Math.max(startYear, endYear)
+				end: isNaN(endYear) ? 2000 : Math.max(startYear, endYear),
+				color: document.querySelector("#input_event_color").value,
 			});
 
 			this.state.timeline["events"].sort((a, b) => a.start - b.start);
 			this.cancelEditEvent(event);
+			this.drawCanvas();
 			event.preventDefault();
+		}
+
+		drawCanvas() {
+			const canvas = document.querySelector("#canvas_timeline");
+			const canvasContext = canvas.getContext("2d");
+			canvas.width = 0;
+			canvas.height = 0;
+			const width = canvas.clientWidth;
+			const height = canvas.clientHeight;
+			canvas.width = width;
+			canvas.height = height;
+			canvasContext.textBaseline = "middle";
+			canvasContext.font = "bold 2em Noto Sans";
+
+			const events = this.state.timeline["events"];
+			let timelineStart = Infinity, timelineEnd = -Infinity;
+			events.forEach(event => {
+				timelineStart = Math.min(event["start"], timelineStart);
+				timelineEnd = Math.max(event["end"], timelineEnd);
+			});
+
+			function getCoordinateFromBounds(position, start, end, width) {
+				return width * (position - start) / (end - start);
+			}
+
+			[...Array(events.length)].forEach((u, index) => {
+				const event = events[index];
+				const startCoordinate = getCoordinateFromBounds(event["start"], timelineStart, timelineEnd, width);
+				const endCoordinate = getCoordinateFromBounds(event["end"], timelineStart, timelineEnd, width);
+
+				canvasContext.fillStyle = event.color;
+				canvasContext.fillRect(startCoordinate, index * EVENT_HEIGHT, endCoordinate - startCoordinate, EVENT_HEIGHT);
+
+				const text = `${event["name"]} (${this.getFormattedYear(event["start"])} – ${this.getFormattedYear(event["end"])})`;
+				const textWidth = canvasContext.measureText(text).width;
+				canvasContext.outlineStyle = "#000000";
+				canvasContext.fillStyle = "#FFFFFF";
+				canvasContext.strokeText(text, Math.min(startCoordinate, width - textWidth), (index + 0.5) * EVENT_HEIGHT);
+				canvasContext.fillText(text, Math.min(startCoordinate, width - textWidth), (index + 0.5) * EVENT_HEIGHT);
+			});
+
+		}
+
+		getFormattedYear(year) {
+			if (year > 0) {
+				return `AD ${year}`
+			} else {
+				return `${-year + 1} BC`
+			}
 		}
 
 		menuBar(timelineLoaded) {
@@ -102,8 +156,8 @@ function setup() {
 									<tbody>
 									{[...Array(events.length)].map((u, index) => (
 										<tr key={`event_${index}`}>
-											<td className="fill_width no_left_padding">{events[index].name}</td>
-											<td className="dates">{events[index].start} - {events[index].end}</td>
+											<td className="fill_width no_left_padding">{events[index]["name"]}</td>
+											<td className="dates">{this.getFormattedYear(events[index]["start"])} – {this.getFormattedYear(events[index]["end"])}</td>
 											<td><a onClick={(event) => this.editEvent(event, index)}>Edit</a></td>
 											<td><a onClick={(event) => this.deleteEvent(event, index)}>Delete</a></td>
 										</tr>
@@ -124,11 +178,11 @@ function setup() {
 								<textarea
 									className="input_text"
 									id="input_event_description"
-									placeholder="Event Description"
+									placeholder="Event Description (optional)"
 								/>
 								<br/>
 								<input
-									className="input_text half_width"
+									className="input_text one_third_width"
 									id="input_event_start"
 									type="text"
 									placeholder="Start Year"
@@ -136,19 +190,24 @@ function setup() {
 									onChange={this.editNumeric}
 								/>
 								<input
-									className="input_text half_width"
+									className="input_text one_third_width"
 									id="input_event_end"
 									type="text"
 									placeholder="End Year"
 									required={true}
 									onChange={this.editNumeric}
 								/>
+								<input
+									className="input_color one_third_width"
+									id="input_event_color"
+									type="color"
+								/>
 								<br/>
 								<input className="input_button half_width" type="submit" value="Submit"/>
 								<input className="input_button half_width" type="button" onClick={this.cancelEditEvent} value="Cancel"/>
 							</form>
 						</div>
-						<canvas/>
+						<canvas id="canvas_timeline"/>
 					</div>
 				);
 			} else {
